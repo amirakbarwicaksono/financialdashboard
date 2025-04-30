614a84af3cb781b3dc11685c2221b88d5c0208e0
amirakbarwicaksono <amirakbarwicaksono@gmail.com>
2025-04-30 10:55:50 +0700

    Added Login Pages

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@
     import { revalidatePath } from 'next/cache';
     import { redirect } from 'next/navigation';
     import postgres from 'postgres'
+    import { signIn } from '@/auth';
+    import { AuthError } from 'next-auth';
 
     const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
     const FormSchema = z.object({
@@ -78,3 +80,21 @@
         revalidatePath('/dashboard/invoices');
     }
   
+    export async function authenticate(
+      prevState: string | undefined,
+      formData: FormData,
+    ) {
+      try {
+        await signIn('credentials', formData);
+      } catch (error) {
+        if (error instanceof AuthError) {
+          switch ((error as any).type) {
+            case 'CredentialsSignin':
+              return 'Invalid credentials.';
+            default:
+              return 'Something went wrong.';
+          }
+        }
+        throw error;
+      }
+    }
